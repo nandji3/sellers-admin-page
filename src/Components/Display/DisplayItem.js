8c9521052d940158427fdbdba81e957409e14925
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-07-23 00:52:10 +0530

    Added **Edit** functionality and fix bug

--- src/Components/Display/DisplayItem.js
+++ src/Components/Display/DisplayItem.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-const DisplayItem = ({ formData, deleteItem, editItems, hrStyle }) => {
+const DisplayItem = ({ formData, deleteItem, editItem, hrStyle }) => {
 
     return (
         <div>
             <hr style={{ ...hrStyle, width: "65%" }} />
-            {formData.map((item) => {
+            {formData.map((ele) => {
                 return (
-                    <div key={`${item.productId}-${item.sellingPrice}-${item.productName}`} style={{ margin: "15px 0px" }}>
-                        <li id={`${item.productId}-${item.sellingPrice}-${item.productName}`}>
-                            Product ID : {item.productId}  --  Selling Price : {item.sellingPrice}  --  Poduct Name : {item.productName}
-                            &nbsp;&nbsp;&nbsp;<button className="edit-item" onClick={() => editItems(`${item.productId}-${item.sellingPrice}-${item.productName}`)}>Edit Item</button>
-                            &nbsp;&nbsp;&nbsp;<button className="delete-item" onClick={() => deleteItem(`${item.productId}-${item.sellingPrice}-${item.productName}`)}>Delete Item</button>
+                    <div key={ele.id} style={{ margin: "15px 0px" }}>
+                        <li id={ele.id}>
+                            Product ID : {ele.productId}  --  Selling Price : {ele.sellingPrice}  --  Poduct Name : {ele.productName}
+                            &nbsp;&nbsp;&nbsp;<button className="edit-item" onClick={() => editItem(ele.id)}>Edit Item</button>
+                            &nbsp;&nbsp;&nbsp;<button className="delete-item" onClick={() => deleteItem(ele.id)}>Delete Item</button>
                         </li>
                     </div>
                 );
